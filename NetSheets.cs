85a76642731ca0e6c27617e33cc6f0d1b669dfd8
Bonnie Jean Kirkpatrick <bonnie@pushthelimits.net>
2015-02-01 16:00:46 -0800

    2.2

--- NetSheets.cs
+++ NetSheets.cs
@@ -7,39 +7,43 @@ public class NetSheets
    // Main method begins execution of C# app
    public static void Main(string[] args)
    {
-      // loop that keeps the application running until the user chooses to close it
-      do{
-      decimal netSheetPrice; // declare the Net Sheet price amount
-      decimal locationMultiplier; // declare the Location Multiplier
-      decimal unitCostTotal; // declare the Internal Marketplace Unit Cost Total from the Test Dealer quote
-      decimal slabListPrice;  // declare the Internal Marketplace Slab List Price from the Test Dealer quote
-      decimal specialPrice; // declare the Net Sheet special pricing
+      do  // loop that keeps the application running until the user chooses to close it
+      {
+         decimal netSheetPrice; // declare the Net Sheet price amount
+         decimal locationMultiplier; // declare the Location Multiplier
+         decimal unitCostTotal; // declare the Internal Marketplace Unit Cost Total from the Test Dealer quote
+         decimal slabListPrice;  // declare the Internal Marketplace Slab List Price from the Test Dealer quote
+         decimal specialPrice; // declare the Net Sheet special pricing
 
-      // prompt user for the net sheet price and read the number
-      Console.Write("\n\nEnter the Net Sheet Price: ");
-      netSheetPrice = Convert.ToDecimal(Console.ReadLine());
+         // prompt user for the net sheet price and read the number
+         Console.WriteLine();
+         Console.Write("Enter the Net Sheet Price: ");
+         netSheetPrice = Convert.ToDecimal(Console.ReadLine());
 
-      // prompt user for the dealer location cost multiplier and read the number
-      Console.Write("Enter the Dealer Location Cost Multiplier: ");
-      locationMultiplier = Convert.ToDecimal(Console.ReadLine());
+         // prompt user for the dealer location cost multiplier and read the number
+         Console.Write("Enter the Dealer Location Cost Multiplier: ");
+         locationMultiplier = Convert.ToDecimal(Console.ReadLine());
 
-      // prompt user for the internal mp unit cost total and read the number
-      Console.Write("Enter the Internal MP Unit Cost Total (branch Test Dealer): ");
-      unitCostTotal = Convert.ToDecimal(Console.ReadLine());
+         // prompt user for the internal mp unit cost total and read the number
+         Console.Write("Enter the Internal MP Unit Cost Total (branch Test Dealer): ");
+         unitCostTotal = Convert.ToDecimal(Console.ReadLine());
 
-      // prompt user for the internal mp slab list price and read the number
-      Console.Write("Enter the Internal MP Slab List Price (branch Test Dealer): ");
-      slabListPrice = Convert.ToDecimal(Console.ReadLine());
+         // prompt user for the internal mp slab list price and read the number
+         Console.Write("Enter the Internal MP Slab List Price (branch Test Dealer): ");
+         slabListPrice = Convert.ToDecimal(Console.ReadLine());
 
-      //calculate Net Sheet special pricing
-      specialPrice = (slabListPrice - (unitCostTotal - (netSheetPrice / locationMultiplier)));
+         //calculate Net Sheet special pricing
+         specialPrice = (slabListPrice - (unitCostTotal - (netSheetPrice / locationMultiplier)));
 
-      // display Net Sheet special pricing
-      Console.WriteLine("\n\nSpecial Net Sheet Pricing is:\n\n\n {0}", Decimal.Round(specialPrice, 2));
+         // display Net Sheet special pricing
+         Console.WriteLine();
+         Console.WriteLine("Special Net Sheet Pricing is: {0:C}", specialPrice);
 
-      // insert two line breaks and tell the user how to continue or close the app
-      Console.WriteLine("\n\nPress 'Enter' to continue or 'X' to close");
-        } //end loop
-while(Console.ReadKey(true).Key != ConsoleKey.X); // condition for the loop; it runs until the user presses the 'X' key
+         // insert two line breaks and tell the user how to continue or close the app
+         Console.WriteLine("Press 'Enter' to continue or 'X' to close");
+      } //end loop
+
+       // condition for the loop; runs until user presses 'X' key
+      while (Console.ReadKey(true).Key != ConsoleKey.X);
    } // end Main
 } // end class NetSheets
